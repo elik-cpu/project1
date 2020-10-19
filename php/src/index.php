feec1a9cc41dd60fb892f7a846c7e6f4bf7e8ce1
elikm5432@gmail.com <elikm5432@gmail.com>
2020-10-19 17:07:15 +0400

    My First Commit

---
+++ php/src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-image: url('https://1news.az/images/2019/04/29/20190429050435810/vbcvbcvb.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  background-size: 100% 100%;
+}
+</style>
+</head>
+<body>
+
+<h2>Web Server in Docker
+<p>Kapital Bank Birinci Bank.</p>
+
+</body>
+</html>
+ 
+
+
